4eecb2578bb98a04e29e3a6238f16d9d1cf6af91
pltnv123 <dotasymphony@gmail.com>
2022-12-22 16:26:15 +0300

    Change web-site

--- auto_screen_page_poisk_porn.py
+++ auto_screen_page_poisk_porn.py
@@ -5,9 +5,11 @@ from selenium.webdriver.common.keys import Keys
 from time import sleep
 
 driver = webdriver.Chrome("C:\dev\chromedriver.exe")
-driver.get("https://google.com")
+driver.get("http://130.193.37.179/app/pets")
 # driver.find_element(By.XPATH, "/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input")
-driver.find_element(By.XPATH, "//input[@title=\'Поиск\']").send_keys('Porn' + Keys.RETURN)
+t = (driver.find_element(By.XPATH, "//*[@id=\"image\"]/img")).click()
+# driver.find_element(By.XPATH, "//input[@title=\'Поиск\']").send_keys('Porn' + Keys.RETURN)
+print(t)
 sleep(2)
-driver.save_screenshot('sf.png')
+# driver.save_screenshot('sf.png')
 driver.quit()
