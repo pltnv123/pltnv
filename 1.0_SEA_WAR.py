6c61068251ba95be1800f8e8bdab9840d13cafa7
pltnv123 <dotasymphony@gmail.com>
2023-01-13 11:21:25 +0300

    fix bag 51str

--- 1.0_SEA_WAR.py
+++ 1.0_SEA_WAR.py
@@ -51,11 +51,7 @@ class Ship:
                 cur_x += i
 
             elif self.o == 1:   # 1 -- Y
-<<<<<<< HEAD
                 cur_y += i
-=======
-                cur_y += 1
->>>>>>> origin/main
 
             ship_dots.append(Dot(cur_x, cur_y))
         return ship_dots
