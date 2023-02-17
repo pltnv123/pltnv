b22b8dc3b7b7638c31da5a8a7e43179638f72b54
pltnv123 <dotasymphony@gmail.com>
2023-02-17 12:43:12 +0300

    Fixed filter in custom_filters

--- django_project_news/news/appnews/templatetags/custom_filters.py
+++ django_project_news/news/appnews/templatetags/custom_filters.py
@@ -24,38 +24,39 @@ def filter_message(message: str):
     message: значение, которое мы получаем от пользователя
      return  - возвращает отфильтрованное от нецензурной лексики сообщение
     """
-    variants = ['сука', 'блять', 'пиздец', 'бля', 'нахуй', 'ебало',
-                'ебальник', 'ебать', 'выебываться', 'долбаеб',
-                'уебан', 'уебался', 'дрочила', 'пидор', 'пизда',
-                'хуета', 'хуй', 'хуйня', 'хуевый', 'хуёвый', 'залупа',
-                'дрочить', 'гандон', 'манда', 'блядь',
-                ]  # непристойные выражения
+    if str(message):
+        variants = ['сука', 'блять', 'пиздец', 'бля', 'нахуй', 'ебало',
+                    'ебальник', 'ебать', 'выебываться', 'долбаеб',
+                    'уебан', 'уебался', 'дрочила', 'пидор', 'пизда',
+                    'хуета', 'хуй', 'хуйня', 'хуевый', 'хуёвый', 'залупа',
+                    'дрочить', 'гандон', 'манда', 'блядь',
+                    ]  # непристойные выражения
 
-    ln = len(variants)
-    filtred_message = ''
-    string = ''
-    pattern = '*'  # чем заменять непристойные выражения
-    str(message)
-    for i in message:
-        string += i
-        string2 = string.lower()
+        ln = len(variants)
+        filtred_message = ''
+        string = ''
+        pattern = '*'  # чем заменять непристойные выражения
+        str(message)
+        for i in message:
+            string += i
+            string2 = string.lower()
 
-        flag = 0
-        for j in variants:
-            if not string2 in j:
-                flag += 1
-            if string2 == j:
-                filtred_message += string[0] + pattern * (len(string) - 1)
-                flag -= 1
+            flag = 0
+            for j in variants:
+                if not string2 in j:
+                    flag += 1
+                if string2 == j:
+                    filtred_message += string[0] + pattern * (len(string) - 1)
+                    flag -= 1
+                    string = ''
+
+            if flag == ln:
+                filtred_message += string
                 string = ''
 
-        if flag == ln:
+        if string2 != '' and string2 not in variants:
             filtred_message += string
-            string = ''
-
-    if string2 != '' and string2 not in variants:
-        filtred_message += string
-    elif string2 != '':
-        filtred_message += pattern * len(string)
+        elif string2 != '':
+            filtred_message += pattern * len(string)
 
     return filtred_message
